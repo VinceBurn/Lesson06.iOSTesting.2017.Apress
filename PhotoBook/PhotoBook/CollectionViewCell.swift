21ef9521e27cf37587cfe61217cb7a0cbb0410e2
Abhishek Mishra <amishra@asmtechnology.com>
2016-12-25 16:17:34 +0000

    user interface built.

---
+++ PhotoBook/PhotoBook/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  PhotoBook
+//
+//  Created by Abhishek Mishra on 23/12/2016.
+//  Copyright © 2016 ASM Technology Ltd. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var shotDetailsLabel: UILabel!
+    
+}
